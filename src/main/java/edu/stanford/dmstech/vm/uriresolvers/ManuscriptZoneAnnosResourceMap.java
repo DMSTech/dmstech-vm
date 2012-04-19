77ba2134c7cfcf56253cebd926dfca0c07d1494d
James Chartrand <jc.chartrand@gmail.com>
2012-04-19 13:00:10 -0400

    Changed maunscript zone annos aggregation to pull from triplestore rather than file

--- src/main/java/edu/stanford/dmstech/vm/uriresolvers/ManuscriptZoneAnnosResourceMap.java
+++ src/main/java/edu/stanford/dmstech/vm/uriresolvers/ManuscriptZoneAnnosResourceMap.java
@@ -3,9 +3,12 @@ package edu.stanford.dmstech.vm.uriresolvers;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 import edu.stanford.dmstech.vm.Config;
+import edu.stanford.dmstech.vm.DMSTechRDFConstants;
 import edu.stanford.dmstech.vm.SharedCanvasUtil;
 
 
@@ -16,9 +19,11 @@ public class ManuscriptZoneAnnosResourceMap {
 	public Response getRepresentation(
 			@PathParam("collectionId") final String collectionId,
 			@PathParam("manuscriptId") final String manuscriptId,
-			@PathParam("fileName") final String requestedFileName
+			@Context UriInfo uriInfo
 			) throws Exception {
-		return SharedCanvasUtil.getSerializedRDFFromHomeDir(Config.collectionSubDir + "/" + collectionId + "/" + manuscriptId + "/rdf/" + Config.zoneAnnotationFileName, requestedFileName);
+		String originalRequest = uriInfo.getAbsolutePath().toASCIIString();
+		
+		return SharedCanvasUtil.buildResourceMapForManuscriptAnnotations(originalRequest, "ZoneAnnotation",  DMSTechRDFConstants.getInstance().scZoneAnnotationListClass);
 			
 	}
 	
